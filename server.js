d79c01e89c1edfb7dc251282e749bf46a1571a8b
liorwen <zear19st@gmail.com>
2018-03-22 17:09:05 +0800

    add node path

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express'),
     fs = require('fs'),
+    path = require('path'),
     app = express();
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/',function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(8080, ip);
