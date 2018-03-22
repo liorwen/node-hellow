a04722b654ee736e5ded2e07ee3289943b16848f
liorwen <zear19st@gmail.com>
2018-03-22 16:23:27 +0800

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+    fs = require('fs'),
+    app = express();
+const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/',function (req, res) {
+    res.send('Hello world' + new Date());
+});
+
+app.listen(8080, ip);
+
+module.exports = app;
