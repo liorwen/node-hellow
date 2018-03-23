afc4bd67d9bdc9adfe4ccb014969731445f3a60e
liorwen <zear19st@gmail.com>
2018-03-23 10:34:13 +0800

    setting static folder

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const Vue = require('vue')
-const server = require('express')()
+const express = require('express');
+const server = express();
+server.use(express.static(__dirname+'/public'));
 const renderer = require('vue-server-renderer').createRenderer({
     template: require('fs').readFileSync('./index.template.html', 'utf-8')
 })
+
 server.get('*', (req, res) => {
     const app = new Vue({
         data: {
             url: req.url
         },
-        template: `    <div>访问的 URL 是： {{ url }}</div>
-                       <img src="img/bg.jpg" alt="風景照">
-                `
+        template: `    <div>
+                            <h3>访问的 URL 是： {{ url }}</h3>
+                            <img src="img/bg.jpg" alt="風景照">
+                       </div>`
     })
     const context = {
         title: 'hello-world',
     }
     renderer.renderToString(app, context, (err, html) => {
         if (err) {
-            res.status(500).end('Internal Server Error')
+            res.status(500).end('Internal Server Error'+err)
             return
         }
         res.end(html);
     })
-})
+});
+
 server.listen(8080)
